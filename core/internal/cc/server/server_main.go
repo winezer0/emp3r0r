4c3841844070ab825dd5ace3f6f6113ca7d60866
jm33-m0 <jm33@jm33.me>
2025-02-23 22:49:55 +1100

    feat: tar config dir for operator to use when server starts

--- core/internal/cc/server/server_main.go
+++ core/internal/cc/server/server_main.go
@@ -1,11 +1,45 @@
 package server
 
-import "github.com/jm33-m0/emp3r0r/core/internal/cc/modules"
+import (
+	"fmt"
+	"os"
+
+	"github.com/jm33-m0/emp3r0r/core/internal/cc/modules"
+	"github.com/jm33-m0/emp3r0r/core/lib/logging"
+	"github.com/jm33-m0/emp3r0r/core/lib/util"
+)
+
+var (
+	Emp3r0rWorkingDir string
+	ConfigTar         string
+)
+
+func init() {
+	// logging
+	logging.SetOutput(os.Stderr)
+
+	// paths
+	home, err := os.UserHomeDir()
+	if err != nil {
+		logging.Fatalf("Failed to get user home directory: %v", err)
+	}
+	Emp3r0rWorkingDir = fmt.Sprintf("%s/.emp3r0r", home)
+	ConfigTar = fmt.Sprintf("%s/emp3r0r.tar.xz", home)
+}
 
 func ServerMain(port int) {
 	// start all services
 	go StartTLSServer()
 	go KCPC2ListenAndServe()
 	go modules.InitModules()
+	go tarConfig()
 	StartMTLSServer(port)
+}
+
+func tarConfig() {
+	// tar all config files
+	err := util.TarXZ(Emp3r0rWorkingDir, ConfigTar)
+	if err != nil {
+		logging.Errorf("Failed to tar config files: %v", err)
+	}
 }
